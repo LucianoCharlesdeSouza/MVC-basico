a6e6c9b0b8b8cf92c08581f5f2b6311e70ea7702
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-07-15 21:55:19 -0300

    Update errorController.php

--- painel/controllers/errorController.php
+++ painel/controllers/errorController.php
@@ -1,8 +1,10 @@
 <?php
 
-class errorController extends Controller {
+class errorController extends Controller 
+{
 
-    public function index() {
+    public function index() 
+    {
         $data = [];
 
         $this->loadTemplate('error_404', $dados);
