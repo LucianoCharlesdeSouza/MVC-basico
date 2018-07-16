7dcaaef412b83a5e5620498c7e3c689636e5c7f4
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-07-15 22:00:04 -0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -2,4 +2,7 @@
     <div class="col-md-4 col-md-offset-3">
         <img src="<?php echo base_url() ?>/assets/img/capa-min.png" width="600" height="100"/>
     </div>
+    <div class="col-md-4 col-md-offset-3">
+        <?php echo html('ola');
+    </div>
 </div>
