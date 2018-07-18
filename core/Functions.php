294e3242fc5f00d2222afdb3823268224abc01dd
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-07-18 12:53:21 -0300

    Update Functions.php

--- core/Functions.php
+++ core/Functions.php
@@ -1,8 +1,14 @@
 <?php
 
+/*
+ * Função que retorna o array de configurações do arquivo database.php
+ * da pasta config, conforme o valor do indice environment do arquivo
+ * environment.php da mesma pasta
+ */
 if (!function_exists('database')) {
 
-    function database($key = null) {
+    function database($key = null)
+    {
 
         $app = include 'config/database.php';
 
@@ -16,9 +22,14 @@ if (!function_exists('database')) {
 
 }
 
+/*
+ * Função que retorna o array de configurações do arquivo environment.php
+ * da pasta config
+ */
 if (!function_exists('environment')) {
 
-    function environment($key) {
+    function environment($key)
+    {
 
         $app = include 'config/environment.php';
 
@@ -27,9 +38,13 @@ if (!function_exists('environment')) {
 
 }
 
+/*
+ * Função que retorna a URL do projeto
+ */
 if (!function_exists('base_url')) {
 
-    function base_url($path = null) {
+    function base_url($path = null)
+    {
 
         $url = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
 
@@ -42,9 +57,13 @@ if (!function_exists('base_url')) {
 
 }
 
+/*
+ * Função que retorna uma URL
+ */
 if (!function_exists('back_url')) {
 
-    function back_url($path = null) {
+    function back_url($path = null)
+    {
 
         if ($path == null) {
             return str_replace(basename(dirname($_SERVER['SCRIPT_FILENAME'])), '', base_url());
@@ -55,6 +74,10 @@ if (!function_exists('back_url')) {
 
 }
 
+/*
+ * Função que aplica o htmlentities() para proibir a execução de scripts
+ * maliciosos nas views
+ */
 if (!function_exists('html')) {
 
     function html($data)
@@ -62,6 +85,18 @@ if (!function_exists('html')) {
         $data = str_replace('<script>', '', str_replace('</script>', '', str_replace('<?php', '', str_replace('?>', '', $data))));
 
         return htmlentities($data, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+    }
+
+}
+
+/*
+ * Função que retorna o valor do REQUEST (GET/POST) caso exista
+ */
+if (!function_exists('requestValue')) {
+
+    function requestValue($name)
+    {
+        return (isset($_REQUEST[$name])) ? $_REQUEST[$name] : '';
     }
 
 }
