b877fcd52fd407fe1a76efbce76c4a8fd94bc254
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-08-10 00:11:53 -0300

    Delete home.php

--- views/home.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-    <div class="col-md-4 col-md-offset-3">
-        <img src="<?php echo base_url() ?>/assets/img/capa-min.png" width="600" height="100"/>
-    </div>
-    <div class="col-md-4 col-md-offset-3">
-        <?php echo html('ola');?>
-    </div>
-</div>
