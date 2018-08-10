38975462e949eb31e5736175317e37a05b69f432
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-08-10 00:40:46 -0300

    Add files via upload

---
+++ core/Controller.php
@@ -0,0 +1,59 @@
+<?php
+
+class Controller
+{
+
+    protected $request;
+
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+        $this->request = new Request();
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     */
+    public function __call($name, $arguments)
+    {
+        $this->loadTemplate('notFound');
+    }
+
+    /**
+     * Carrega uma view fora do Template,
+     * podendo ou não receber dados
+     * @param $viewName
+     * @param array $viewData
+     */
+    public function loadView($viewName, $viewData = array())
+    {
+        extract($viewData);
+        include 'views/' . $viewName . '.php';
+    }
+
+    /**
+     * Carrega o arquivo de Template,
+     * @param $viewName
+     * @param array $viewData
+     */
+    public function loadTemplate($viewName, $viewData = array())
+    {
+        include 'views/template.php';
+    }
+
+    /**
+     * Carrega as views dentro do Template,
+     * podendo ou não receber dados
+     * @param $viewName
+     * @param $viewData
+     */
+    public function loadViewInTemplate($viewName, $viewData)
+    {
+        extract($viewData);
+        include 'views/' . $viewName . '.php';
+    }
+
+}
---
+++ core/Core.php
@@ -0,0 +1,51 @@
+<?php
+
+class Core
+{
+
+    /**
+     * Método responsável por resolver os dados da URL
+     */
+    public function run()
+    {
+        $url = '/';
+        if (isset($_GET['url'])) {
+            $url .= rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+        }
+
+        $params = array();
+        if (!empty($url) && $url != '/') {
+            $url = explode('/', $url);
+            array_shift($url);
+
+            $currentController = $url[0] . 'Controller';
+            array_shift($url);
+
+            if (isset($url[0]) && !empty($url[0])) {
+                $currentAction = $url[0];
+                array_shift($url);
+            } else {
+                $currentAction = 'index';
+            }
+
+            if (count($url) > 0) {
+                $params = $url;
+            }
+        } else {
+            $currentController = 'homeController';
+            $currentAction = 'index';
+        }
+
+        if (!file_exists('controllers/' . $currentController . '.php') || !method_exists($currentController, $currentAction)):
+            $c = new notFoundController();
+            $currentAction = 'index';
+            http_response_code(404);
+        else:
+            $c = new $currentController();
+        endif;
+
+        call_user_func_array(array($c, $currentAction), $params);
+    }
+
+}
