d04ab7f1396d4c929bf4a7a9ff58112acf7e1a52
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-08-11 23:22:54 -0300

    Update install.php

--- views/make/install.php
+++ views/make/install.php
@@ -205,6 +205,7 @@
                     </div>
                     <div class="row"><br/></div>
                     <div class="col-md-4  offset-md-4">
+                        <br />
                         <button class="btn btn-lg btn-primary btn-block install" type="submit">
                             <i class="setup "></i>
                             Finish settings
