08e07fbfbbba026f73fe42270655a4c2e28bda08
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-08-14 21:08:19 -0300

    Update Ajax.php

--- system/Functions/Ajax.php
+++ system/Functions/Ajax.php
@@ -13,3 +13,26 @@ if (!function_exists('boxAjax')) {
 
 }
 
+/*
+ * Função que retorna os atributos necessário ao motor Ajax
+ */
+if (!function_exists('ajaxForm')) {
+
+    function ajaxForm($controller, $class = null)
+    {
+        return "class='ajaxForm " . $class . "' data-controller='" . $controller . "'";
+    }
+
+}
+
+/*
+ * Função que retorna botão usado na requisição Ajax
+ */
+if (!function_exists('btnAjaxForm')) {
+
+    function btnAjaxForm($value, $class = null)
+    {
+        return '<button class="' . $class . '"><i class="btnAjaxForm fa "></i>' . $value . '</button>';
+    }
+
+}
