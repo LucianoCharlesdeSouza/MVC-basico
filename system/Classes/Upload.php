bfbdcad7462a2ca689959a906f842ccf72fa4768
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-08-17 21:41:58 -0300

    Update Upload.php

--- system/Classes/Upload.php
+++ system/Classes/Upload.php
@@ -24,7 +24,7 @@ class Upload
     private $allowed = []; //array com os mime types permitidos
     private $height = 800; //800px
     private $width = 1546; //1546px
-    private $size = 250000; //1.5MB;
+    private $size = 450000; //4.5MB;
     private $path; //caminho onde será salvo a imagem
     private $error;
 
