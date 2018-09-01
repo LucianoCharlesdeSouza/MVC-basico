3a435d4de1f6c3083b5bdaea8c29ee6c9121e724
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-09-01 12:10:29 -0300

    Update template.php

--- views/template.php
+++ views/template.php
@@ -4,6 +4,8 @@
         <!-- Meta tags Obrigatórias -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="<?php echo base_url("/assets/css/bootstrap.min.css"); ?>">
