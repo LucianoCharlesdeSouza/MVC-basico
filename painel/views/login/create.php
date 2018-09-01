9eb6ccb6aac12f047aa5e35c438884216afeb857
Luciano Charles de Souza <souzacomprog@gmail.com>
2018-08-31 23:02:26 -0300

    Update create.php

--- painel/views/login/create.php
+++ painel/views/login/create.php
@@ -1,99 +1,27 @@
-<!Doctype html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<form class="form-signin" method="post" action="#">
 
-        <title><?php echo isset($viewData['title']) ? $viewData['title'] : 'Cadastro de Usuário'; ?></title>
+    <div class="col-md-12 text-center">
+        <img class="text-center mb-4" src="<?php echo back_url("/views/make/logo.png"); ?>" alt="" width="72" height="72">
+    </div>
 
-        <!-- Bootstrap core CSS -->
-        <link href="<?php echo base_url("/assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
+    <h3 class="text-center h3 mb-3 font-weight-normal">Insira seus dados para o cadastro</h3>
 
-        <style>
-            html,
-            body {
-                height: 100%;
-            }
+    <label for="inputName" class="sr-only">Nome...</label>
+    <input type="text" name="user_name" id="inputName" class="form-control" placeholder="Nome"  autofocus>
 
-            body {
-                display: -ms-flexbox;
-                display: flex;
-                -ms-flex-align: center;
-                align-items: center;
-                padding-top: 40px;
-                padding-bottom: 40px;
-                background-color: #fff;
-            }
+    <label for="inputEmail" class="sr-only">Email...</label>
+    <input type="text" name="user_email" id="inputEmail" class="form-control" placeholder="E-mail"  autofocus>
 
-            .form-signin {
-                width: 100%;
-                max-width: 330px;
-                padding: 15px;
-                margin: auto;
-            }
-            .form-signin .checkbox {
-                font-weight: 400;
-            }
-            .form-signin a{
-                color: black;
-            }
-            .form-signin a:hover{
-                color: #0069D9;
-            }
-            .form-signin .form-control {
-                position: relative;
-                box-sizing: border-box;
-                height: auto;
-                padding: 10px;
-                font-size: 16px;
-            }
-            .form-signin .form-control:focus {
-                z-index: 2;
-            }
-            .form-signin input[type="email"] {
-                margin-bottom: -1px;
-                border-bottom-right-radius: 0;
-                border-bottom-left-radius: 0;
-            }
-            .form-signin input[type="password"] {
-                margin-bottom: 10px;
-                border-top-left-radius: 0;
-                border-top-right-radius: 0;
-            }
-        </style>
-    </head>
+    <label for="inputPassword" class="sr-only">Senha...</label>
+    <input type="password" name="user_password" id="inputPassword" class="form-control" placeholder="Password" >
 
-    <body>
+    <div class="checkbox mb-3">
+        <label>
+            <a href="#">Login</a>
+        </label>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Cadastrar</button>
 
-        <form class="form-signin" method="post" action="#">
+    <p class="text-center mt-5 mb-3 text-muted">&copy; MVC-git / 2017-2018</p>
 
-            <div class="col-md-12 text-center">
-                <img class="text-center mb-4" src="<?php echo back_url("/views/make/logo.png"); ?>" alt="" width="72" height="72">
-            </div>
-
-            <h3 class="text-center h3 mb-3 font-weight-normal">Insira seus dados para o cadastro</h3>
-
-            <label for="inputName" class="sr-only">Nome...</label>
-            <input type="text" name="user_name" id="inputName" class="form-control" placeholder="Nome"  autofocus>
-
-            <label for="inputEmail" class="sr-only">Email...</label>
-            <input type="text" name="user_email" id="inputEmail" class="form-control" placeholder="E-mail"  autofocus>
-
-            <label for="inputPassword" class="sr-only">Senha...</label>
-            <input type="password" name="user_password" id="inputPassword" class="form-control" placeholder="Password" >
-
-            <div class="checkbox mb-3">
-                <label>
-                    <a href="#">Login</a>
-                </label>
-            </div>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Cadastrar</button>
-
-            <p class="text-center mt-5 mb-3 text-muted">&copy; MVC-git / 2017-2018</p>
-
-        </form>
-        <script src="<?php echo base_url("/assets/js/jquery.js"); ?>"></script>
-        <script src="<?php echo base_url("/assets/js/bootstrap.min.js"); ?>"></script>
-
-    </body>
-</html>
+</form>
